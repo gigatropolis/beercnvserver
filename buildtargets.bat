524d5e8ada0524074b2bfcf43d213a0e907bbfd3
Kregg Kemper <gigatropolis@yahoo.com>
2018-05-28 17:54:51 -0700

    Update build targets with arm64. Batch file uses -o in go build to copy file to bin directories

--- buildtargets.bat
+++ buildtargets.bat
@@ -1,28 +1,27 @@
 
 set GOOS=windows
 set GOARC=amd64
-go build beersvr.go
-copy beersvr.exe bin\beersvr_win_amd64
+go build -o bin\beersvr_win_amd64\beersvr.exe beersvr.go
 
 set GOOS=linux
 set GOARC=amd64
-go build beersvr.go
-copy beersvr bin\beersvr_linux_amd64
+go build -o bin\beersvr_linux_amd64\beersvr beersvr.go
 
 set GOOS=linux
 set GOARC=386
-go build beersvr.go
-copy beersvr bin\beersvr_linux_386
+go build -o bin\beersvr_linux_386\beersvr beersvr.go
 
 set GOOS=linux
 set GOARC=arm
-go build beersvr.go
-copy beersvr bin\beersvr_linux_arm
+go build -o bin\beersvr_linux_arm\beersvr beersvr.go
+
+set GOOS=linux
+set GOARC=arm64
+go build -o bin\beersvr_linux_arm64\beersvr beersvr.go
 
 set GOOS=darwin
 set GOARC=arm64
-go build beersvr.go
-copy beersvr bin\beersvr_darwin_amd64
+go build -o bin\beersvr_darwin_amd64\beersvr beersvr.go
 
 set GOOS=windows
 set GOARC=amd64
